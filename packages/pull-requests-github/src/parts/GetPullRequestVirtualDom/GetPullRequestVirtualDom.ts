6b94673cfcabec7fa9e65eb2d6bf0177a361ec22
Le Vivilet <levivilet@pm.me>
2026-07-22 10:14:42 +0000

    chore: update eslint and eslint config to latest

--- packages/pull-requests-github/src/parts/GetPullRequestVirtualDom/GetPullRequestVirtualDom.ts
+++ packages/pull-requests-github/src/parts/GetPullRequestVirtualDom/GetPullRequestVirtualDom.ts
@@ -4,22 +4,34 @@ import type { PullRequestViewState } from '../PullRequestViewState/PullRequestVi
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import { renderStatus } from '../RenderStatus/RenderStatus.ts'
 
+const viewNode: VirtualDomNode = {
+  childCount: 2,
+  className: 'PullRequestView',
+  type: VirtualDomElements.Div,
+}
+
+const formNode: VirtualDomNode = {
+  childCount: 2,
+  className: 'PullRequestForm',
+  name: 'pullRequestForm',
+  onSubmit: DomEventListenerFunctions.HandleSubmit,
+  type: VirtualDomElements.Form,
+}
+
+const buttonNode: VirtualDomNode = {
+  childCount: 1,
+  className: 'PullRequestButton',
+  name: 'loadPullRequest',
+  onClick: DomEventListenerFunctions.HandleClick,
+  type: VirtualDomElements.Button,
+}
+
 export const getPullRequestVirtualDom = (state: PullRequestViewState): readonly VirtualDomNode[] => {
   const { url } = state
   const statusDom = renderStatus(state)
   return [
-    {
-      childCount: 2,
-      className: 'PullRequestView',
-      type: VirtualDomElements.Div,
-    },
-    {
-      childCount: 2,
-      className: 'PullRequestForm',
-      name: 'pullRequestForm',
-      onSubmit: DomEventListenerFunctions.HandleSubmit,
-      type: VirtualDomElements.Form,
-    },
+    viewNode,
+    formNode,
     {
       childCount: 0,
       className: 'PullRequestInput',
@@ -31,13 +43,7 @@ export const getPullRequestVirtualDom = (state: PullRequestViewState): readonly 
       type: VirtualDomElements.Input,
       value: url,
     },
-    {
-      childCount: 1,
-      className: 'PullRequestButton',
-      name: 'loadPullRequest',
-      onClick: DomEventListenerFunctions.HandleClick,
-      type: VirtualDomElements.Button,
-    },
+    buttonNode,
     text('Load'),
     ...statusDom,
   ]
--- packages/pull-requests-github/src/parts/RenderField/RenderField.ts
+++ packages/pull-requests-github/src/parts/RenderField/RenderField.ts
@@ -1,24 +1,24 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 
+const fieldNode: VirtualDomNode = {
+  childCount: 2,
+  className: 'PullRequestField',
+  type: VirtualDomElements.Div,
+}
+
+const labelNode: VirtualDomNode = {
+  childCount: 1,
+  className: 'PullRequestFieldLabel',
+  type: VirtualDomElements.Div,
+}
+
+const valueNode: VirtualDomNode = {
+  childCount: 1,
+  className: 'PullRequestFieldValue',
+  type: VirtualDomElements.Div,
+}
+
 export const renderField = (label: string, value: string): readonly VirtualDomNode[] => {
-  return [
-    {
-      childCount: 2,
-      className: 'PullRequestField',
-      type: VirtualDomElements.Div,
-    },
-    {
-      childCount: 1,
-      className: 'PullRequestFieldLabel',
-      type: VirtualDomElements.Div,
-    },
-    text(label),
-    {
-      childCount: 1,
-      className: 'PullRequestFieldValue',
-      type: VirtualDomElements.Div,
-    },
-    text(value || '-'),
-  ]
+  return [fieldNode, labelNode, text(label), valueNode, text(value || '-')]
 }
