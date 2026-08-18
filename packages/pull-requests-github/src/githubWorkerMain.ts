bce15c05f8320dbd03d19ad84113f301da817367
Le Vivilet <levivilet@pm.me>
2026-08-18 03:25:13 -0700

    Use host-managed RPC for GitHub worker (#23)

--- packages/pull-requests-github/src/githubWorkerMain.ts
+++ packages/pull-requests-github/src/githubWorkerMain.ts
@@ -1,6 +1,6 @@
-import { WebWorkerRpcClient2 } from '@lvce-editor/rpc'
+import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import { commandMap } from './parts/GitHubWorkerCommandMap/GitHubWorkerCommandMap.ts'
 
-await WebWorkerRpcClient2.create({
+await WebWorkerRpcClient.create({
   commandMap,
 })
