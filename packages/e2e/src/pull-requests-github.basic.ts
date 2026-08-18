c0a3beeacfa2859b606887f5d1b089daa7d8e541
Le Vivilet <levivilet@pm.me>
2026-08-18 03:24:26 -0700

    feature: add pull requests view icon (#22)

--- packages/e2e/src/pull-requests-github.basic.ts
+++ packages/e2e/src/pull-requests-github.basic.ts
@@ -4,6 +4,9 @@ export const name = 'pull-requests-github.empty-description'
 
 export const test: Test = async ({ Command, expect, Locator }) => {
   await Command.executeExtensionCommand('PullRequestsGithub.show')
+  const icon = Locator('.ActivityBarItem[title="Pull Requests"] .MaskIcon')
+  await expect(icon).toBeVisible()
+  await expect(icon).toHaveCSS('mask-image', `url("${location.origin}/icons/git-pull-request.svg")`)
   const input = Locator('input[name="pullRequestUrl"]')
   await expect(input).toBeVisible()
 }
