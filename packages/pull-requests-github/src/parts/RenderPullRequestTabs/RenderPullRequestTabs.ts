899cd8d09f9f584245b36b2adc0c020fff092216
Le Vivilet <levivilet@pm.me>
2026-08-18 14:41:53 +0000

    feature: extract pull request tab renderer

--- packages/pull-requests-github/src/parts/RenderPullRequestTabs/RenderPullRequestTabs.ts
+++ packages/pull-requests-github/src/parts/RenderPullRequestTabs/RenderPullRequestTabs.ts
@@ -1,8 +1,8 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import { AriaRoles, mergeClassNames, text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import { AriaRoles, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { PullRequestFilter } from '../PullRequestFilter/PullRequestFilter.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as PullRequestFilters from '../PullRequestFilter/PullRequestFilter.ts'
+import { renderPullRequestTab } from '../RenderPullRequestTab/RenderPullRequestTab.ts'
 
 const tabListNode: VirtualDomNode = {
   ariaLabel: 'Pull request state',
@@ -12,41 +12,10 @@ const tabListNode: VirtualDomNode = {
   type: VirtualDomElements.Div,
 }
 
-const renderTab = (filter: PullRequestFilter, activeFilter: PullRequestFilter, count: number): readonly VirtualDomNode[] => {
-  const active = filter === activeFilter
-  const label = filter === PullRequestFilters.Open ? 'Open' : 'Closed'
-  const name = filter === PullRequestFilters.Open ? 'showOpenPullRequests' : 'showClosedPullRequests'
-  return [
-    {
-      ariaSelected: active,
-      childCount: 2,
-      className: mergeClassNames('PullRequestTab', active ? 'PullRequestTabActive' : ''),
-      name,
-      onClick: DomEventListenerFunctions.HandleClick,
-      role: AriaRoles.Tab,
-      tabIndex: active ? 0 : -1,
-      type: VirtualDomElements.Button,
-    },
-    {
-      childCount: 1,
-      name,
-      type: VirtualDomElements.Span,
-    },
-    text(label),
-    {
-      childCount: 1,
-      className: 'PullRequestTabCount',
-      name,
-      type: VirtualDomElements.Span,
-    },
-    text(String(count)),
-  ]
-}
-
 export const renderPullRequestTabs = (activeFilter: PullRequestFilter, openCount: number, closedCount: number): readonly VirtualDomNode[] => {
   return [
     tabListNode,
-    ...renderTab(PullRequestFilters.Open, activeFilter, openCount),
-    ...renderTab(PullRequestFilters.Closed, activeFilter, closedCount),
+    ...renderPullRequestTab(PullRequestFilters.Open, activeFilter, openCount),
+    ...renderPullRequestTab(PullRequestFilters.Closed, activeFilter, closedCount),
   ]
 }
