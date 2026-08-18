c287504fae35350ee784ce853649ec90e79952b4
Le Vivilet <levivilet@pm.me>
2026-08-18 09:42:08 +0000

    fix: load pull request extension in dev

---
+++ packages/build/test/dev.test.ts
@@ -0,0 +1,21 @@
+import { deepEqual, match } from 'node:assert/strict'
+import { readFile } from 'node:fs/promises'
+import { join } from 'node:path'
+import { test } from 'node:test'
+import { getServerArgs } from '../src/getServerArgs.ts'
+import { root } from '../src/root.ts'
+
+test('loads the development extension', () => {
+  deepEqual(getServerArgs(), [
+    'node_modules/@lvce-editor/server/bin/server.js',
+    '--only-extension=packages/pull-requests-github',
+    '--test-path=packages/e2e',
+  ])
+})
+
+test('writes the development bundle next to the extension manifest', async () => {
+  const content = await readFile(join(root, 'package.json'), 'utf8')
+  const packageJson = JSON.parse(content)
+
+  match(packageJson.scripts['build:watch'], /--outfile=packages\/pull-requests-github\/dist\/pullRequestWorkerMain\.js(?:\s|$)/)
+})
