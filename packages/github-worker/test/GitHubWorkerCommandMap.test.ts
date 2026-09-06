adcdfacfff10b2cabfec912c6592b1d17522be89
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-06 21:51:12 +0000

    fix: compare registered commands independent of order

--- packages/github-worker/test/GitHubWorkerCommandMap.test.ts
+++ packages/github-worker/test/GitHubWorkerCommandMap.test.ts
@@ -2,19 +2,21 @@ import { expect, test } from '@jest/globals'
 import { commandMap } from '../src/parts/GitHubWorkerCommandMap/GitHubWorkerCommandMap.ts'
 
 test('exposes github logic over rpc', () => {
-  expect(Object.keys(commandMap)).toEqual([
-    'GitHub.createRequest',
-    'GitHub.setCreateResponses',
-    'GitHub.getCreateRequests',
-    'GitHub.clearPullRequestData',
-    'GitHub.fetchPullRequest',
-    'GitHub.fetchPullRequests',
-    'GitHub.setPullRequestData',
-    'GitHub.setPullRequestError',
-    'GitHub.setPullRequestListData',
-    'GitHub.setPullRequestListError',
-    'GitHub.setPullRequestListResponse',
-    'GitHub.setPullRequestResponse',
-    'GitHub.validatePullRequestUrl',
-  ])
+  expect(Object.keys(commandMap).toSorted()).toEqual(
+    [
+      'GitHub.createRequest',
+      'GitHub.setCreateResponses',
+      'GitHub.getCreateRequests',
+      'GitHub.clearPullRequestData',
+      'GitHub.fetchPullRequest',
+      'GitHub.fetchPullRequests',
+      'GitHub.setPullRequestData',
+      'GitHub.setPullRequestError',
+      'GitHub.setPullRequestListData',
+      'GitHub.setPullRequestListError',
+      'GitHub.setPullRequestListResponse',
+      'GitHub.setPullRequestResponse',
+      'GitHub.validatePullRequestUrl',
+    ].toSorted(),
+  )
 })
